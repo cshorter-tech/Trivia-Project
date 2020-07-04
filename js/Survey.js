bfd04ece86dd6b2238613a88925eb20b793337bf
Charles Shorter <cshorter-tech@gmail.com>
2020-07-03 22:04:35 -0400

    added rendering for components 50%

--- js/Survey.js
+++ js/Survey.js
@@ -1,22 +1,22 @@
-export default function Quiz(questions) {
+export default function Survey(questions) {
     this.questions = questions;
     this.score = 0;
     this.currentIndex = 0;
 }
 
-Quiz.prototype.getCurrentQuestion = function () {
+Survey.prototype.getCurrentQuestion = function () {
     return this.questions[this.currentIndex];
 }
 
-Quiz.prototype.nextIndex = function () {
+Survey.prototype.nextIndex = function () {
     this.currentIndex++;
 }
 
-Quiz.prototype.lastIndex = function () {
+Survey.prototype.lastIndex = function () {
     return this.currentIndex === this.questions.length;
 }
 
-Quiz.prototype.guess == function (userInput) {
+Survey.prototype.guess == function (userInput) {
     const currentQuestion = this.questions[this.currentIndex]
     if (currentQuestion.isCorrect(userInput)) {
         this.score++
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import Question from "./Questions.js"
-import Quiz from "./Quiz.js"
+import Survey from "./Survey.js"
 
 // Revealing Module Pattern via IFFE
 const App = (function () {
@@ -14,7 +14,7 @@ const App = (function () {
     const restartButtonEl = document.querySelector(".restart")
 
     // Question Intializers
-    const q1 = new Question("What is 1+1?", [1, 2, 3, 4], 1);
+    const q1 = new Question("How many 'original' musketeers are there?", ["One", "Two", "Three", "Four"], 2);
     const q2 = new Question("Who is the king of Pop?", ["Martin Luther King", "Michael Jackson", "Prince", "Earth, Wind & Fire"], 1);
     const q3 = new Question("Who is the first president of the US?", ["Abe Lincoln", "Obama", "George Washington", "George Bush"], 2);
     const q4 = new Question("When was JavaScript created?", ["1800s", "2020", "1990", "1995"], 3);
@@ -22,9 +22,73 @@ const App = (function () {
     const q6 = new Question("What is the capital of France?", ["Panama", "Washington, DC", "London", "Paris"], 3);
     const q7 = new Question("What is considered traditional to Japanese cuisine?", ["Sushi", "Frog Legs", "Burgers", "Mash & Potatoes"], 0);
 
-    const qArray = [q1, q2, q3, q4, q5, q6, q7];
-    const survey = new Quiz(qArray);
-    console.log(survey)
+    const surveyArray = [q1, q2, q3, q4, q5, q6, q7];
+    const survey = new Survey(surveyArray);
+    // console.log(survey)
+
+    // Render Components
+    const setValue = (elem, value) => {
+        elem.innerHTML = value;
+    }
+
+
+    const renderQuestion = _ => {
+        const question = survey.getCurrentQuestion().question;
+        // surveyQuestionEl.innerHTML = question;
+        setValue(surveyQuestionEl, question)
+
+    }
+    // renderQuestion();
+
+    const renderChoices = _ => {
+        let markup = "";
+        const currentChoices = survey.getCurrentQuestion().choices;
+        currentChoices.forEach((elem, index) => {
+            // console.log(elem, index)
+            markup += `
+            <li class="choice">
+              <input
+                type="radio"
+                name="choice"
+                class="survey__input"
+                id="choice${index}"
+              />
+              <label for="choice${index}" class="survey__label">
+                <i></i>
+                <span>${elem}</span>
+              </label>
+            </li>
+            `
+        })
+        // surveyChoicesEl.innerHTML = markup;
+        setValue(surveyChoicesEl, markup)
+
+
+    }
+    // renderChoices();
+
+    const renderTracker = _ => {
+
+    }
+
+
+    const renderAll = () => {
+        if (survey.lastIndex()) {
+            // 1. render end quiz modale
+        } else {
+            // 1. render suvery question
+            renderQuestion();
+            // 2. render survey choices
+            renderChoices();
+            // 3. render survey tracker
+            // 4. render survey bar-inner
+
+
+        }
+    }
+    renderAll()
+
+    // console.log(survey.lastIndex());
 
     // Front End logic
     let counter = 0;
@@ -49,9 +113,9 @@ const App = (function () {
     };
 })();
 
-console.log(App.get());
-App.set(2);
-console.log(App.get());
+// console.log(App.get());
+// App.set(2);
+// console.log(App.get());
 
 
 
@@ -74,4 +138,3 @@ console.log(App.get());
 // console.log(myQuiz.getCurrentQuestion());
 
 // // check if quiz ended
-// console.log(myQuiz.lastIndex());
